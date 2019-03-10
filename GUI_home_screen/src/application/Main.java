e503f06c4086f0fb4cf9679765d0fe1a846de414
JackSmall2401 <44839912+JackSmall2401@users.noreply.github.com>
2019-03-10 21:21:32 +0000

    Update Main.java
    comments

--- GUI_home_screen/src/application/Main.java
+++ GUI_home_screen/src/application/Main.java
@@ -15,10 +15,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage){
 		try {
+			// build main GUI home screen from fxml file that was build in scene builder
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			
+			// set to a scene
 			Scene scene = new Scene(root,800,500);
+			// apply css file (style)
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// display GUI home page
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
@@ -28,6 +31,7 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		// launch app
 		launch(args);
 	}
 }
