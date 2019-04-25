149b7a3d64987a95d85dc00c00d6fe6ecb8e4824
JackSmall2401 <44839912+JackSmall2401@users.noreply.github.com>
2019-04-25 12:17:55 +0100

    Add files via upload

--- NewChuseMaster/src/listDataStructure/Result.java
+++ NewChuseMaster/src/listDataStructure/Result.java
@@ -4,35 +4,37 @@ import java.util.ArrayList;
 
 import cloudInteraction.UserAccount;
 
+/**
+ * class of holding a set of results from a pass of the comparison algorithm  
+ * 
+ * Date created: 22/04/2019
+ * Date last edited: 22/04/2019
+ * Last edited by: Jack and Luke
+ * 
+ * @author Jack and Luke
+ *
+ */
 public class Result{
 	
+	// the user that the results are associated 
 	private UserAccount user;
 	
+	// holds the sets of results the user made 
 	private ArrayList<RankingList> results;
 	
 	public Result(UserAccount user, ArrayList<RankingList> results){
 		
 		this.user = user;
-		
-		this.setResults(results);
-		
-		
+		this.results = results;
 	}
 
+	// get user
 	public UserAccount getUser() {
 		return user;
 	}
 
-	public void setUser(UserAccount user) {
-		this.user = user;
-	}
-
+	// get all the ranking list (results)
 	public ArrayList<RankingList> getRankingList() {
 		return results;
 	}
-
-	public void setResults(ArrayList<RankingList> results) {
-		this.results = results;
-	}
-		
 }
--- NewChuseMaster/src/listDataStructure/StatisticsDataStructure.java
+++ NewChuseMaster/src/listDataStructure/StatisticsDataStructure.java
@@ -2,12 +2,6 @@ package listDataStructure;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collection;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.WildcardFileFilter;
-
-import backEnd.BackEndContainer;
 import cloudInteraction.CloudInteractionHandler;
 import cloudInteraction.UserAccount;
 import xmlHandling.XMLHandler;
@@ -21,47 +15,47 @@ public class StatisticsDataStructure{
 	/**
 	 * This constructs all data on list across multiple files for the statistics
 	 * 
+	 * Date created: 25/04/2019
+	 * Date last edited: 25/04/2019
+	 * Last edited by: Jack and Luke
+ 	 * 
 	 * @param file_path
 	 */
 	public StatisticsDataStructure(String file_path){
 		
-		// 1 get name from file path DONE
-		// 2 chuse list from file path DONE
-		// 3 get results from file path DONE 
-		
-		// 4 search for files with same name (title) DONE
-		// 5 add each users results with user details DONE
-		
+		// set the chuse list from the list witch is being passed by file_path
 		list = new ChuseList();
 		list = XMLHandler.buildListFromXML(file_path);
+		// set list name form name in file path
 		list.setListName(setTitleFromFilePath(file_path));
 		
+		// holds the result form a file
 		ArrayList<RankingList> temp = new ArrayList<RankingList>();
 		
+		// get results from the XML file given by file path
 		temp = XMLHandler.buildResultsListFromXML(file_path);
 
-		//result_list.add(new Result(BackEndContainer.getLocalAccount(),temp)); 
-		//result_list.add(new Result(new UserAccount("Jack@mail.com", "Jack", "22", "Male"), temp)); 
+		// set results array
 		result_list = new ArrayList<Result>();
 		
-		// This is for testing purposes
+		// This gets the information needed for the statistics from the cloud, using the username that is extracted from the XML
 		String gender = CloudInteractionHandler.queryUserAccountByProperty("username", list.getAuthor()).next().getString("gender");
 		String age = CloudInteractionHandler.queryUserAccountByProperty("username", list.getAuthor()).next().getString("age");
+		// This creates the results file from the data previously collected
 		addResult(new UserAccount("null", list.getAuthor(), age, gender), temp);
-		// The actual code when in use with the application
-		// addResult(BackEndContainer.getLocalAccount(), temp);
-		
+		// This is to extract the usernames from the XML results files name e.g XML-user.xml returns user
 		ArrayList<String> results_files = getResultsFilesExtension(file_path.substring(0, file_path.indexOf("\\saves")+6), list.getName().substring(0, list.getName().indexOf(".xml")));
-		
+		// This stores the user account
 		UserAccount temp_user;
-		
+		// For each result file get the information needed for statistics from the cloud and create the user account
 		for (int j = 0; j < results_files.size(); j++){
 			gender = CloudInteractionHandler.queryUserAccountByProperty("username", results_files.get(j)).next().getString("gender");
 			age = CloudInteractionHandler.queryUserAccountByProperty("username", results_files.get(j)).next().getString("age");
-			
+			// Creates the user account class that will be stored in the results class
 			temp_user = new UserAccount("null", results_files.get(j), age, gender);
-			
+			// reconstructs the file path in order to get the results list
 			temp = XMLHandler.buildResultsListFromXML(file_path.substring(0, file_path.indexOf(".xml")) +"-"+ results_files.get(j)+".xml");
+			// adds the results list and user account to the results class
 			addResult(temp_user, temp);
 		}
 		
@@ -92,11 +86,14 @@ public class StatisticsDataStructure{
 	public static ArrayList<String> getResultsFilesExtension(String directory_name, String extension){
 		
 		File folder = new File(directory_name);
+		// Finds all files within the saves folder
 		String[] all_files = folder.list();
-		ArrayList<String> files = new ArrayList();
+		// This stores the relevant files name
+		ArrayList<String> files = new ArrayList<String>();
 		int i;
-
+		
 		for (i = 0; i < folder.list().length; i++){
+			// If the selected file is a results file for the list, then add to the list of the files
 			if (all_files[i].contains(extension+"-")){
 				files.add(all_files[i].substring(all_files[i].indexOf("-")+1, all_files[i].indexOf(".xml")));
 			}
