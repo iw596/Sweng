d0e31c0b894099364f59a3b46180de48fc27f11e
iw596 <43037941+iw596@users.noreply.github.com>
2019-04-27 19:21:15 +0100

    added return statements

--- NewChuseMaster/src/spotifydata/GetPlaylistsTracks.java
+++ NewChuseMaster/src/spotifydata/GetPlaylistsTracks.java
@@ -36,66 +36,71 @@ public class GetPlaylistsTracks {
 	      .build();
 	  }
 
-	  public static void getPlaylistsTracks_Sync() {
-	    try {
-	      final Paging<PlaylistTrack> playlistTrackPaging = getPlaylistsTracksRequest.execute();
+	  public static ArrayList<BasicItem> getPlaylistsTracks_Sync() {
+		    try {
+		      final Paging<PlaylistTrack> playlistTrackPaging = getPlaylistsTracksRequest.execute();
+		      
+		      
+		      System.out.println(getPlaylistsTracksRequest.execute());
+		      //System.out.println(playlistTrackPaging.getLimit());
+		      System.out.println(playlistTrackPaging);
+		      
+		      System.out.println("Total: " + playlistTrackPaging.getTotal());
+		      System.out.println("Href: " + playlistTrackPaging.getHref());
 
-	      System.out.println(getPlaylistsTracksRequest.execute());
-	      //System.out.println(playlistTrackPaging.getLimit());
-	      System.out.println(playlistTrackPaging);
-	      
-	      System.out.println("Total: " + playlistTrackPaging.getTotal());
-	      System.out.println("Href: " + playlistTrackPaging.getHref());
+		      int playlist_length;
+		      
+		      if(playlistTrackPaging.getTotal() > 100) {
+		    	  playlist_length = 100;
+		      } else {
+		    	  playlist_length = playlistTrackPaging.getTotal();
+		      }
+		      
+		      for (int i= 0; i < playlist_length; i++) {
+		    	  System.out.println("Track "+ i + " name: " + playlistTrackPaging.getItems()[i].getTrack().getName());
+		    	  System.out.println(playlistTrackPaging.getItems()[i].getTrack().getPreviewUrl());
+		      }
+		      
+		      ArrayList<BasicItem> spotify_items = new ArrayList<BasicItem>();
+		      
+		      //loops through every file path in the list 
+		      for(int i = 0; i < playlist_length; i++ ) {
+		    	  //adds every file path to the audio item array list
+		    	  
+		    	  ArrayList<String> track_metadata = new ArrayList<String>();
+		    	  
+		    	  track_metadata.add(playlistTrackPaging.getItems()[i].getTrack().getName());
+		    	  
+		    	  String artist = playlistTrackPaging.getItems()[i].getTrack().getArtists()[0].getName();
+		    	  
+		    	  if(playlistTrackPaging.getItems()[i].getTrack().getArtists().length > 1) {
+		    		  for(int j=1; j < playlistTrackPaging.getItems()[i].getTrack().getArtists().length; j++) {
+		    			  artist += ", " + playlistTrackPaging.getItems()[i].getTrack().getArtists()[j].getName();
+		    		  }
+		    	  }
+		    	  
+		    	  track_metadata.add(artist);
+		    	  track_metadata.add(playlistTrackPaging.getItems()[i].getTrack().getAlbum().getName());
+		    	  track_metadata.add(Double.toString(((double)playlistTrackPaging.getItems()[i].getTrack().getDurationMs() / 1000) / 60));
 
-	      int playlist_length;
-	      
-	      if(playlistTrackPaging.getTotal() > 100) {
-	    	  playlist_length = 100;
-	      } else {
-	    	  playlist_length = playlistTrackPaging.getTotal();
-	      }
-	      
-	      for (int i= 0; i < playlist_length; i++) {
-	    	  System.out.println("Track "+ i + " name: " + playlistTrackPaging.getItems()[i].getTrack().getName());
-	      }
-	      
-	      ArrayList<BasicItem> spotify_items = new ArrayList<BasicItem>();
-	      
-	      //loops through every file path in the list 
-	      for(int i = 0; i < playlist_length; i++ ) {
-	    	  //adds every file path to the audio item array list
-	    	  
-	    	  ArrayList<String> track_metadata = new ArrayList<String>();
-	    	  
-	    	  track_metadata.add(playlistTrackPaging.getItems()[i].getTrack().getName());
-	    	  
-	    	  String artist = playlistTrackPaging.getItems()[i].getTrack().getArtists()[0].getName();
-	    	  
-	    	  if(playlistTrackPaging.getItems()[i].getTrack().getArtists().length > 1) {
-	    		  for(int j=1; j < playlistTrackPaging.getItems()[i].getTrack().getArtists().length; j++) {
-	    			  artist += ", " + playlistTrackPaging.getItems()[i].getTrack().getArtists()[j].getName();
-	    		  }
-	    	  }
-	    	  
-	    	  track_metadata.add(artist);
-	    	  track_metadata.add(playlistTrackPaging.getItems()[i].getTrack().getAlbum().getName());
-	    	  track_metadata.add(Double.toString(((double)playlistTrackPaging.getItems()[i].getTrack().getDurationMs() / 1000) / 60));
+		    	  
+		    	  spotify_items.add(new SpotifyItem(playlistTrackPaging.getItems()[i].getTrack().getId(), track_metadata,playlistTrackPaging.getItems()[i].getTrack().getPreviewUrl(), playlistTrackPaging.getItems()[i].getTrack().getAlbum().getImages()[0]));
+		      }
+		      // System.out.println(playlistTrackPaging.getItems());
+		      
+		      
+		      for(BasicItem item : spotify_items) {
+		    	  item.print();
+		      }
+		      return spotify_items;
+		      
+		      //System.out.println("Track 1 name: " + playlistTrackPaging.getItems()[1].getTrack().getName());
+		    } catch (IOException | SpotifyWebApiException e) {
+		    	System.out.println("Error: " + e.getMessage());
+		    }
+		    
+		    return null;
 
-	    	  
-	    	  spotify_items.add(new SpotifyItem(playlistTrackPaging.getItems()[i].getTrack().getId(), track_metadata));
-	      }
-	      // System.out.println(playlistTrackPaging.getItems());
-	      
-	      
-	      for(BasicItem item : spotify_items) {
-	    	  item.print();
-	      }
-	      
-	      //System.out.println("Track 1 name: " + playlistTrackPaging.getItems()[1].getTrack().getName());
-	    } catch (IOException | SpotifyWebApiException e) {
-	    	System.out.println("Error: " + e.getMessage());
-	    }
-
-	  }
+		  }
 
 }
