adea50888826bea50006c193326b71de1548a2bd
JackSmall2401 <44839912+JackSmall2401@users.noreply.github.com>
2019-04-30 10:56:52 +0100

    Add files via upload

--- NewChuseMaster/src/listDataStructure/StatisticsDataStructure.java
+++ NewChuseMaster/src/listDataStructure/StatisticsDataStructure.java
@@ -17,6 +17,7 @@ public class StatisticsDataStructure{
 	private ArrayList<Result> result_list;
 	
 	/**
+	 * This is an empty constructor for use in making a filtered data structure
 	 * 
 	 */
 	public StatisticsDataStructure(){
@@ -29,7 +30,7 @@ public class StatisticsDataStructure{
 	 * This constructs all data on list across multiple files for the statistics
 	 * 
 	 * Date created: 25/04/2019
-	 * Date last edited: 25/04/2019
+	 * Date last edited: 30/04/2019
 	 * Last edited by: Jack and Luke
  	 * 
 	 * @param file_path
@@ -83,6 +84,7 @@ public class StatisticsDataStructure{
 	 */
 	public static String setTitleFromFilePath(String file_path){
 		
+		// set name from file path
 		String Title = file_path.substring((System.getProperty("user.dir") + "\\saves\\").length());
 		
 		return Title;
@@ -113,98 +115,133 @@ public class StatisticsDataStructure{
 		}
 		return files;
 	}
-	
+	/**
+	 * add new result to result list
+	 * 
+	 * 
+	 * @param user
+	 * @param results
+	 */
 	public void addResult(UserAccount user, ArrayList<RankingList> results){
-
+		// add new result
 		this.result_list.add(new Result(user, results));
 		
 	}
 	
+	/**
+	 * get original list
+	 * 
+	 * @return
+	 */
 	public ChuseList getList(){
 		return list;
 	}
 	
+	/**
+	 * get result list, which each result has user's information and their ranked lists 
+	 * 
+	 * @return
+	 */
 	public ArrayList<Result> getResultList(){
 		return result_list;
 	}
 	
+	/**
+	 * 
+	 * Gets the results corresponding to an age range
+	 * 
+	 * @param low
+	 * @param high
+	 * @return
+	 */
 	public StatisticsDataStructure getDataForGivenAgeRange(int low, int high){
 		
+		// Build new statistics data structure
 		StatisticsDataStructure ageGroupResults = new StatisticsDataStructure();
+		// set chuse list
 		ageGroupResults.list = this.list;
+		// temp result for checking 
 		Result result;
 		
+		// look at all users 
 		for(int i = 0; i < this.getResultList().size(); i++){
+			// if users age is between the age bounds add them to the new data structure 
 			if((this.getResultList().get(i).getUser().getAge() >= low) 
 				&& (this.getResultList().get(i).getUser().getAge() <= high)){
 				
+				// set them to the result
 				result = this.getResultList().get(i);
+				// add the user and the ranking list to the new data structure
 				ageGroupResults.getResultList().add(result);
 			}
 		}
 		
+		// return new data structure
 		return ageGroupResults;
 	}
 	
+	/**
+	 * 
+	 * Gets the results corresponding to a gender
+	 * 
+	 * @param gender
+	 * @return
+	 */
 	public StatisticsDataStructure getDataForGivenGender(String gender){
 		
+		// Build new statistics data structure
 		StatisticsDataStructure genderGroupResults = new StatisticsDataStructure();
+		// set chuse list
 		genderGroupResults.list = this.list;
 		
+		
+		// look at all users 
 		for(int i = 0; i < this.getResultList().size(); i++){
+			// checks that the user is the set gender
 			if(this.getResultList().get(i).getUser().getGender().equals(gender)){
+				// add user to the new statistics data structure with all there information and ranking lists 
 				genderGroupResults.getResultList().add(this.getResultList().get(i));
 			}
 		}
 		
+		// return new data structure
 		return genderGroupResults;
 	}
 	
+	/**
+	 * 
+	 * Gets the results for a given time range
+	 * 
+	 * @param start
+	 * @param end
+	 * @return
+	 */
 	public StatisticsDataStructure getDataForGivenTimeRange(Date start, Date end){
 		
+		// Build new statistics data structure
 		StatisticsDataStructure timeRangeResults = new StatisticsDataStructure();
+		// set chuse list
 		timeRangeResults.list = this.list;
-
 				
 		//fuck_you_java_fuck_you_stuart_this_elec_cource_why_do_fucking_programing_read_the_fuck_departments_name_you_dip_shit;
 		
-		/*for(int i = 0; i < this.getResultList().size(); i++){
-			timeRangeResults.getResultList().add(this.getResultList().get(i));
-		}
-		
-		for(int i = 0; i < this.getResultList().size(); i++){
-			for(int j = 0; j < this.getResultList().get(i).getRankingList().size(); j++){
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-				
-				Date temp;
-				
-				try {
-					temp = sdf.parse(this.getResultList().get(i).getRankingList().get(j).getTime().substring(0, 10));
-					if ((start.compareTo(temp) > 0) || (end.compareTo(temp) < 0)){
-						timeRangeResults.getResultList().get(i).getRankingList().remove(j);
-					}
-					
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				
-			}
-		}*/
-		
 		Result temp;
-		
+		// look at all users
 		for(int i = 0; i < this.getResultList().size(); i++){
+			// created a temporary results structure, containing the user and an empty array for the results.
 			temp = new Result(this.getResultList().get(i).getUser(), new ArrayList<RankingList>());
+			// Loops through all results
 			for(int j = 0; j < this.getResultList().get(i).getRankingList().size(); j++){
+				// Date formatting
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 				
 				Date temp_date;
 				
 				try {
+					// get the date that this partular set of results was created on
 					temp_date = sdf.parse(this.getResultList().get(i).getRankingList().get(j).getTime().substring(0, 10));
 					//if (!((start.compareTo(temp_date) > 0) || (end.compareTo(temp_date) < 0))){
+					// If the set of results was made within the time frame, then add it to the temporary results structure
 					if (((start.compareTo(temp_date) <= 0) && end.compareTo(temp_date) >= 0)){
 						temp.getRankingList().add(this.getResultList().get(i).getRankingList().get(j));
 					}
@@ -215,15 +252,25 @@ public class StatisticsDataStructure{
 				}
 				
 			}
+			// Check if any results are added to the temporary structure
 			if(temp.getRankingList().size() != 0){
+				// If results were added,then add this user to the data structure
 				timeRangeResults.getResultList().add(temp);
 			}
 			
 		}
 		
+		// return new data structure
 		return timeRangeResults;
 	}
 	
+	/**
+	 * 
+	 * Gets how many times an item has won
+	 * 
+	 * @param item
+	 * @return
+	 */
 	public int getNumberOfWins(String item){
 		int wins = 0;
 		
@@ -238,10 +285,19 @@ public class StatisticsDataStructure{
 		return wins;
 	}
 	
+	
+	/**
+	 * 
+	 * Gets how many sets of results there are for a given list
+	 * 
+	 * @return
+	 */
 	public int getNumberOfResults(){
 		int results = 0;
 		
+		// goes through the result list
 		for(int i = 0; i < this.getResultList().size(); i++){
+			// goes through the sets of results within the ranking list
 			for(int j = 0; j < this.getResultList().get(i).getRankingList().size(); j++){
 				results++;
 			}
@@ -250,25 +306,44 @@ public class StatisticsDataStructure{
 		return results;
 	}
 	
+	/**
+	 * 
+	 * Gets the average score (mean) of the items in a list
+	 * 
+	 * scores it based on the following formula:
+	 * (size of list - (position-1))
+	 * and then finds the mean over all ranking lists
+	 * 
+	 * @param item
+	 * @return
+	 */
 	public double getAverageScore(String item){
+		// Get number of results
 		int number_of_results = this.getNumberOfResults();
+		// tracking score
 		double score = 0;
+		// makes sure that there are actually results (avoid dividing by 0)
 		if(number_of_results != 0){
+			// gets how many items that are being compared
+			int number_of_items = this.getList().getSize();
 		
-		int number_of_items = this.getList().getSize();
-		
-		for(int i = 0; i < this.getResultList().size(); i++){
-			for(int j = 0; j < this.getResultList().get(i).getRankingList().size(); j++){
-				for(int k = 0; k < this.getResultList().get(i).getRankingList().get(j).getSize(); k++){
-					if(this.getResultList().get(i).getRankingList().get(j).get(k).getWrappedItem().getTitle().equals(item)){
-						score += number_of_items - k;
-						continue;
+			// Goes through all the results
+			for(int i = 0; i < this.getResultList().size(); i++){
+				//Goes through each ranking list for each result
+				for(int j = 0; j < this.getResultList().get(i).getRankingList().size(); j++){
+					// Goes through each item in the ranking list
+					for(int k = 0; k < this.getResultList().get(i).getRankingList().get(j).getSize(); k++){
+						// checks if the item in the list is the one that is being looked for
+						if(this.getResultList().get(i).getRankingList().get(j).get(k).getWrappedItem().getTitle().equals(item)){
+							// sets the score
+							score += number_of_items - k;
+							continue;
+						}
 					}
 				}
 			}
-		}
-		
-		score = score/this.getNumberOfResults();
+			// gets the mean overall
+			score = score/this.getNumberOfResults();
 		}
 		
 		return score;
